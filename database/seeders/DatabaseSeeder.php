39339605df5f4f73c4799022e64c1d9ce56dedde
Jerome Suyat <lhabzter21@gmail.com>
2024-05-22 10:34:05 +0800

    setup database schema and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+            ProductSeeder::class,
+            CategorySeeder::class,
+            CategoryProductSeeder::class,
+        ]);
     }
 }
